e4648959326753d789f2cb8a85db209a33522e0d
dgattoni <danigattoni@gmail.com>
2026-06-22 21:13:59 +1000

    chore: styling portable text components

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,7 +1,15 @@
-import { PortableText, type SanityDocument } from "next-sanity";
+import { PortableText, type PortableTextComponents, type SanityDocument } from "next-sanity";
 import { client } from "@/sanity/client";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+
+const portableTextComponents: PortableTextComponents = {
+  block: {
+    h2: ({ children }) => <h2 className="mt-8 mb-3 text-3xl">{children}</h2>,
+    h3: ({ children }) => <h3 className="mt-6 mb-2 text-2xl">{children}</h3>,
+    h4: ({ children }) => <h4 className="mt-4 mb-2 text-1xl">{children}</h4>,
+  },
+};
 
 const POST_QUERY = `*[_type == "post" && slug.current == $slug][0]`;
  
@@ -27,7 +35,7 @@ export default async function PostPage({
       <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
       <div className="prose">
         <p className="mb-4">Published: {new Date(post.publishedAt).toLocaleDateString()}</p>
-        <div className="content-body">{Array.isArray(post.body) && <PortableText value={post.body} />}</div>
+        <div className="content-body">{Array.isArray(post.body) && <PortableText value={post.body} components={portableTextComponents} />}</div>
       </div>
     </main>
   );
